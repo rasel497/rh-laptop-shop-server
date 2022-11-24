f58152be27c5bd9ec36fab0312205c70e2048d4e
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-11-24 18:10:12 +0600

    initial setup and server running success

---
+++ index.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const cors = require('cors');
+const jwt = require('jsonwebtoken');
+require('dotenv').config();
+
+const app = express();
+const port = process.env.PORT || 5000;
+
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+// Initial and Basic Setup
+app.get('/', (req, res) => {
+    res.send('RH-Laptop-Shop is running...');
+});
+
+app.listen(port, () => {
+    console.log(`RH-Laptop-Shop is running: ${port}`);
+});
