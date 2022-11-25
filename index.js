53fbd27b461793d2e387fbbe7f5ae1d18b55014d
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-11-25 17:20:54 +0600

    get homecategory get from database

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
@@ -9,6 +10,32 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(cors());
 app.use(express.json());
+
+
+// MongoDB connections
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.mpr3cem.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+// console.log(client);
+
+
+// Start MongoDb collection acsess
+async function run() {
+    try {
+        const categoriesCollection = client.db("laptopShop").collection("homeCategories");
+
+
+        app.get('/homeCategories', async (req, res) => {
+            const query = {};
+            const categories = await categoriesCollection.find(query).toArray();
+            res.send(categories);
+        });
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.log);
 
 
 // Initial and Basic Setup
